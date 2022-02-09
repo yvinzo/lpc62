d82213d243a49d6877b3a4a6b74ea512a299a530
Gerardo Bernal C <gerardo.bernal@uanl.edu.mx>
2022-02-08 21:17:46 -0600

    Agregando script de pokemon en python desde Linux.

---
+++ pokemon1.py
@@ -0,0 +1,14 @@
+import requests
+#
+if __name__ == '__main__':
+	url = 'http://pokeapi.co/api/v2/pokemon-form/'
+	
+	response = requests.get(url)
+	if response.status_code == 200:
+		payload = response.json()
+		results = payload.get('results',[])
+		
+		if results:
+			for pokemon in results:
+				name = pokemon['name']
+				print(name)
